9875b9932db2542eebb25793b712b91c8fec0800
18092816731 <251990765@qq.com>
2018-05-17 19:21:01 +0800

    500

--- public/index.php
+++ public/index.php
@@ -16,7 +16,8 @@ header('Access-Control-Allow-Origin:*');
 // 响应类型  
 header('Access-Control-Allow-Methods:*');  
 // 响应头设置  
-header('Access-Control-Allow-Headers:*');  
+header('Access-Control-Allow-Headers:content-type,token');  
+header("Access-Control-Request-Headers: Origin, X-Requested-With, content-Type, Accept");
 
 // 定义应用目录
 define('APP_PATH', __DIR__ . '/../application/');
