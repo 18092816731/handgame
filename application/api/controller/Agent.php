ea4294a123ed4187c0055d7b87dad283ec758f7d
18092816731 <251990765@qq.com>
2018-05-21 12:18:30 +0800

    1

--- application/api/controller/Agent.php
+++ application/api/controller/Agent.php
@@ -16,7 +16,16 @@ class Agent
     {
         $this->agent = new \app\api\model\Agent();
         $this->userCard  = new \app\api\model\AgentCard();
-        //验证
+/*         //验证token
+        if(!array_key_exists('HTTP_TOKEN',$_SERVER))
+        {
+            return  return_json(2,'用户token不存在或已过期');
+        }
+        if(!array_key_exists('HTTP_ID',$_SERVER))
+        {
+            return  return_json(2,'用户不存在或已过期');
+        } */
+        
     }
     /**
      * 1-1 新增代理账号
